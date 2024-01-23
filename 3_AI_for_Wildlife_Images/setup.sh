777ebe22e0dc3e5529f3bd7f5cf328ebc8cbfe08
Santiago Martinez <santiago.mbal@gmail.com>
2024-01-23 06:48:32 +0000

    Added echo commands to setup script

--- 3_AI_for_Wildlife_Images/setup.sh
+++ 3_AI_for_Wildlife_Images/setup.sh
@@ -1,38 +1,50 @@
 #!/bin/env bash
 
-# Cloning required repos
+echo "Cloning required repositories"
 if [ ! -d "models" ]; then
-	git clone --depth 1 https://github.com/tensorflow/models
+	echo -n "Cloning tensorflow/models repository"
+	git clone --depth 1 https://github.com/tensorflow/models >>git.log 2>&1
+	echo " - Done"
 fi
 if [ ! -d "CameraTraps" ]; then
-	git clone https://github.com/microsoft/CameraTraps
-    cd CameraTraps
-    git checkout v5.0
-    cd ..
+	echo -n "Cloning CameraTraps repository"
+	git clone https://github.com/microsoft/CameraTraps >>git.log 2>&1
+	cd CameraTraps
+	git checkout v5.0 >>git.log 2>&1
+	cd ..
+	echo " - Done"
 fi
 if [ ! -d "ai4eutils" ]; then
-	git clone https://github.com/microsoft/ai4eutils
+	echo -n "Cloning ai4eutils repository"
+	git clone https://github.com/microsoft/ai4eutils >>git.log 2>&1
+	echo " - Done"
 fi
 if [ ! -d "yolov5" ]; then
-	git clone https://github.com/ultralytics/yolov5/
-    cd yolov5
-    git checkout c23a441c9df7ca9b1f275e8c8719c949269160d1
-    cd ..
+	echo -n "Cloning yolov5 repository"
+	git clone https://github.com/ultralytics/yolov5/ >>git.log 2>&1
+	cd yolov5
+	git checkout c23a441c9df7ca9b1f275e8c8719c949269160d1 >>git.log 2>&1
+	cd ..
+	echo " - Done"
 fi
+echo " "
 
-# Installing the Object Detection API
+echo -n "Installing the Object Detection API"
 cd models/research
-protoc object_detection/protos/*.proto --python_out=.
-cp object_detection/packages/tf2/setup.py .
-python -m pip install .
+protoc object_detection/protos/*.proto --python_out=. >>objectapi.log 2>&1
+cp object_detection/packages/tf2/setup.py . >>objectapi.log 2>&1
+python -m pip install . >>objectapi.log 2>&1
 cd ../..
+echo " - Done"
 
-# Installing the dependencies
+echo -n "Installing Python dependencies"
 pip install \
 	torch==1.10.1+cu113 \
 	torchvision==0.11.2+cu113 \
-	-f https://download.pytorch.org/whl/cu113/torch_stable.html
-pip install humanfriendly jsonpickle
+	-f https://download.pytorch.org/whl/cu113/torch_stable.html >>install.log 2>&1
+pip install humanfriendly jsonpickle >>install.log 2>&1
+echo " - Done"
 
-# Downloading the model weights
+echo -n "Downloading the model weights"
 wget -O /content/md_v5a.0.0.pt https://github.com/microsoft/CameraTraps/releases/download/v5.0/md_v5a.0.0.pt
+echo " - Done"
