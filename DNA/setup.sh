6b878c2aaef050ed8549df0b9aadfbae9f88b312
mbsantiago <santiago.mbal@gmail.com>
2026-03-20 12:03:57 +0000

    Add basta to setup

--- DNA/setup.sh
+++ DNA/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Download the data and dependencies
 gdown 1_gHXRWcQIlcRma17TkTHp02Dlaz_QBhj
 gdown 14OJtDDdV3PQ6q3HrIQNG7kkTr6iC1myu
 
+# Unzip
 unzip alejando_data_xochimilco.zip -d data
 unzip /content/r_dada_dependencies.zip -d /
 
+# Download the utils.R file
 wget https://github.com/MScEcologyAndDataScienceUCL/BIOS0032_AI4Environment/raw/refs/heads/main/DNA/utils.R
+
+# Install basta
+uv tool install --from https://github.com/timkahlke/BASTA.git --with leveldb --with plyvel --with krona --with wget --python 3.11 basta
