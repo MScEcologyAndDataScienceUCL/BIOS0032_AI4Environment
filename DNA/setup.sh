36d425471c6a5bc9c192153c991ede8adc801e32
mbsantiago <santiago.mbal@gmail.com>
2026-03-20 11:57:25 +0000

    Update setup

--- DNA/setup.sh
+++ DNA/setup.sh
@@ -5,3 +5,5 @@ gdown 14OJtDDdV3PQ6q3HrIQNG7kkTr6iC1myu
 
 unzip alejando_data_xochimilco.zip -d data
 unzip /content/r_dada_dependencies.zip -d /
+
+wget https://github.com/MScEcologyAndDataScienceUCL/BIOS0032_AI4Environment/raw/refs/heads/main/DNA/utils.R
