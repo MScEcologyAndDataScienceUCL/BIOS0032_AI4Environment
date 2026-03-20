e075ef9d453d42eb871743abdaeaf29a256bd992
mbsantiago <santiago.mbal@gmail.com>
2026-03-20 12:44:07 +0000

    Add basta and blast utils

--- DNA/utils.R
+++ DNA/utils.R
@@ -130,3 +130,106 @@ read_taxa_from_basta <- function(path) {
     )) |>
     as.matrix()
 }
+
+
+get_basta_bin <- function() {
+  tool_dir <- system2(
+    "uv",
+    args = c("tool", "dir"),
+    stdout = TRUE
+  )
+
+  paste0(tool_dir, "/basta/bin/basta")
+}
+
+create_blast_db <- function(fasta_path, output_dir) {
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir, recursive = TRUE)
+  }
+
+  system2(
+    "makeblastdb",
+    args = c(
+      "-in",
+      fasta_path,
+      "-parse_seqids",
+      "-dbtype",
+      "nucl",
+      "-out",
+      file.path(output_dir, "blast_db")
+    )
+  )
+}
+
+run_blastn <- function(
+  query_path,
+  db_dir,
+  output_path
+) {
+  db <- file.path(db_dir, "blast_db")
+
+  system2(
+    "blastn",
+    args = c(
+      "-query",
+      query_path,
+      "-db",
+      db,
+      "-outfmt",
+      "'6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore'",
+      "-out",
+      output_path
+    )
+  )
+}
+
+create_basta_db <- function(mapping_path, name) {
+  basta_bin <- get_basta_bin()
+
+  system2(
+    basta_bin,
+    args = c(
+      "create_db",
+      mapping_path,
+      paste0(name, "_mapping.db"),
+      "0",
+      "1"
+    )
+  )
+}
+
+run_basta <- function(
+  output_dir,
+  blast_path,
+  database_name,
+  identity = 98,
+  alength = 200,
+  percent = 80,
+  minimum = 1
+) {
+  basta_bin <- get_basta_bin()
+
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir, recursive = TRUE)
+  }
+
+  system2(
+    basta_bin,
+    args = c(
+      "sequence",
+      "-i",
+      identity,
+      "-l",
+      alength,
+      "-p",
+      percent,
+      "-m",
+      minimum,
+      blast_path,
+      file.path(output_dir, "basta_out.txt"),
+      "-v",
+      file.path(output_dir, "basta_vout.txt"),
+      database_name
+    )
+  )
+}
