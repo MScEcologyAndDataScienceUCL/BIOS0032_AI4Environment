7310ae50b5f24407aba7e10bdd218c04ea9933dc
mbsantiago <santiago.mbal@gmail.com>
2026-03-20 12:37:24 +0000

    Add blast installation

--- DNA/setup.sh
+++ DNA/setup.sh
@@ -4,12 +4,22 @@
 gdown 1_gHXRWcQIlcRma17TkTHp02Dlaz_QBhj
 gdown 14OJtDDdV3PQ6q3HrIQNG7kkTr6iC1myu
 
-# Unzip
+# Unzip and clean up
+unzip r_dada_dependencies.zip -d /
 unzip alejando_data_xochimilco.zip -d data
-unzip /content/r_dada_dependencies.zip -d /
+
+rm r_dada_dependencies.zip
+rm alejando_data_xochimilco.zip
 
 # Download the utils.R file
 wget https://github.com/MScEcologyAndDataScienceUCL/BIOS0032_AI4Environment/raw/refs/heads/main/DNA/utils.R
 
 # Install basta
 uv tool install --from https://github.com/timkahlke/BASTA.git --with leveldb --with plyvel --with krona --with wget --python 3.11 basta
+
+uv tool update-shell
+
+# Install blast
+wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.17.0+-x64-linux.tar.gz
+tar zxvpf ncbi-blast-2.17.0+-x64-linux.tar.gz -C /usr/local/ --strip-components=1
+rm ncbi-blast-2.17.0+-x64-linux.tar.gz
